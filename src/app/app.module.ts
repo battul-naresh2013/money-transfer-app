92a2a2f628b87a64fff03bf48062ef2cd477d54f
Naresh Battul <battul.naresh2013@gmail.com>
2023-06-18 22:08:23 +0200

    added header, footer and logo components for common layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { BbUIModule } from './bb-ui/bb-ui.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/layout/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BbUIModule
   ],
   providers: [],
   bootstrap: [AppComponent]
